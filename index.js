22de0b577afebba709a836af28cc0474922dd7ba
rai8 <hytonne@outlook.com>
2020-07-26 21:02:53 +0300

    adding a member

--- index.js
+++ index.js
@@ -9,8 +9,15 @@ const app = express()
 //initialize middleware
 //app.use(logger)
 
+//body parser middleware
+
+app.use(express.json()) //to handle raw json
+app.use(express.urlencoded({ extended: false }))
+
 //setting a static folder
 app.use(express.static(path.join(__dirname, "public")))
+
+//members API routes
 app.use("/api/members", require("./routes/api/members"))
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
--- routes/api/members.js
+++ routes/api/members.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const router = express.Router()
 const members = require("../../Members")
+const uuid = require("uuid")
 
 //creating a simple rest api--- gets all members
 router.get("/", (req, res) => res.json(members))
@@ -14,6 +15,21 @@ router.get("/:id", (req, res) => {
         members.filter((member) => member.id === parseInt(req.params.id))
       )
     : res.status(400).json({ msg: `No member with id of ${req.params.id}` })
+})
+
+//creating a member--- used to update the database POST request
+router.post("/", (req, res) => {
+  const newMember = {
+    id: uuid.v4(),
+    name: req.body.name,
+    email: req.body.email,
+    status: "active",
+  }
+  if (!newMember.name || !newMember.email) {
+    return res.status(400).json({ msg: "Please include a name and email" })
+  }
+  members.push(newMember)
+  res.json(members)
 })
 
 module.exports = router
