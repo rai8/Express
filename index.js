00ee6b2d40c7d2eb03d297699b437eea9186bd38
rai8 <hytonne@outlook.com>
2020-07-26 18:41:03 +0300

    setting up routes to show a single page

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require("express")
+const path = require("path")
 
 const app = express()
+
+//create a route
+app.get("/", (req, res) => {
+  /* res.send("<h1>Hello world !!</h1>") */
+  res.sendFile(path.join(__dirname, "public", "index.html"))
+})
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
